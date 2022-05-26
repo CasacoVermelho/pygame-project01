16aaaeea0d80ede06505ae3854926e47c814ed00
CasacoVermelho <alexandrebmjr@gmail.com>
2022-05-26 14:47:47 -0300

    drawing with rectangles and colors

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surf = test_font.render('My game', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (600,300))
@@ -28,7 +30,9 @@ while True:
 
 	screen.blit(sky_surface,(0,0))
 	screen.blit(ground_surface,(0,300))
-	screen.blit(text_surface,(300,50))
+	pygame.draw.rect(screen,'#c0e8ec',score_rect)
+	# pygame.draw.rect(screen,'Green',score_rect,10)
+	screen.blit(score_surf,score_rect)
 
 	snail_rect.x -= 4
 	if snail_rect.right < 0: snail_rect.left = 800
